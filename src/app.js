8ed5dd8d3bdf8139b36e321b8b97d148faa240ac
Lutkowski <a.lutkovskiy@audiostore.media>
2025-11-28 08:04:06 +0300

    feat: add global error handler

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import {lessonsRouter} from "./routes/lessons.routes.js";
+import {errorHandler} from "./middlewares/error-handler.js";
 
 export const app = express();
 
 app.use(express.json());
 
 app.use('/lessons', lessonsRouter);
+
+app.use(errorHandler);
--- src/controllers/lessons.controller.js
+++ src/controllers/lessons.controller.js
@@ -2,12 +2,7 @@ import {lessonsService} from "../services/lessons.service.js";
 
 export const lessonsController = {
     async getLessons(req, res) {
-        try {
             const data = await lessonsService.getLessons(req.validatedQuery);
             res.json(data);
-        } catch (err) {
-            console.error(err);
-            res.status(500).json({error: 'Internal server error'});
-        }
     }
 }
