185dd60eacda796632184f8cdea63b8054843d0c
Lutkowski <a.lutkovskiy@audiostore.media>
2025-11-28 07:58:44 +0300

    feat: handling of unexisted routes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import { app } from './app.js';
 
 const PORT = 8080;
 
+app.use((req, res) => {
+    res.status(404).json({ error: 'Not found. Use /lessons endpoint.' });
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
